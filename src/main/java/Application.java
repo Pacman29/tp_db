45717823ada0a7ccecd9dac1e1e6bd7d2f1afb96
pacman29 <balyakin.d.v@gmail.com>
2017-03-16 00:02:34 +0300

    init, add forums

---
+++ src/main/java/Application.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by pacman29 on 15.03.17.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
---
+++ src/main/java/DatabaseServices/ForumsTableService.java
@@ -0,0 +1,8 @@
+package DatabaseServices;
+
+/**
+ * Created by pacman29 on 15.03.17.
+ */
+public final class ForumsTableService {
+    public
+}
